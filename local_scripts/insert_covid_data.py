147d5f9eb290a7031f6275a63530da6872ece982
Bruno Eduardo <bruno.freire@timeenergy.com>
2021-03-08 17:08:56 -0300

    Adicionando script responsavel por adicionar dados no mysql.

--- local_scripts/insert_covid_data.py
+++ local_scripts/insert_covid_data.py
@@ -4,7 +4,7 @@ import COVID19Py
 from mysql.connector import connect
 
 def get_covid_data_by_country(countrie_cody: str = 'BR'):
-    covid19 = COVID19Py.COVID19()
+    covid19 = COVID19Py.COVID19(url="https://cvtapi.nl")
     dict_data = covid19.getLocationByCountryCode(countrie_cody, timelines=True)[0]
     latitude = dict_data['coordinates']['latitude']
     longitude = dict_data['coordinates']['longitude']
@@ -20,15 +20,16 @@ def get_covid_data_by_country(countrie_cody: str = 'BR'):
                 'longitude': longitude, 'acumulated_confirmed_cases': confirmed_timelines[time],
                 'acumulated_deaths': 0}
 
-rdsdb = connect(host=os.environ["RDS_HOST"], user=os.environ["RDS_USER"], password=os.environ["RDS_PASSWORD"], database="covid")
+rdsdb = connect(host=os.environ["RDS_HOST"], user=os.environ["RDS_USER"], password=os.environ["RDS_PASSWORD"], database="covid_cases", port=5432)
 print('Conectado')
 cursor = rdsdb.cursor()
-initial_query = """CREATE TABLE IF NOT EXISTS covid_timeline(measured_at datetime, country_code varchar(10), latitude float, longitude float, acumulated_confirmed_cases integer, acumulated_deaths interger)"""
+initial_query = """CREATE TABLE IF NOT EXISTS covid_timeline(measured_at datetime, country_code varchar(10), latitude float, longitude float, acumulated_confirmed_cases integer, acumulated_deaths integer)"""
 cursor.execute(initial_query)
 
 if __name__ == "__main__":
-    for data in get_confirmed_cases_among_time():
+    for data in get_covid_data_by_country("US"):
+        print(data)
         query = f"""INSERT INTO covid_timeline(measured_at, country_code, latitude, longitude, acumulated_confirmed_cases, acumulated_deaths)
-                    VALUES ({data["timestamp"], data["country_code"], data['latitude'], data['longitude'], data['acumulated_confirmed_cases'], data['acumulated_deaths']})"""
+                    VALUES ('{data["timestamp"]}', '{data["country_code"]}', {data['latitude']}, {data['longitude']}, {data['acumulated_confirmed_cases']}, {data['acumulated_deaths']})"""
         cursor.execute(query)
         time.sleep(0.2)
